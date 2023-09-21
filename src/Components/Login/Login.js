2963b08f081a6e66641fa84f2024a55a2153e50f
laxitsolanki <solankilaxit963@gmail.com>
2023-09-21 11:16:47 +0530

    first header

---
+++ src/Components/Login/Login.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import '../../Components/Login/Login.css';
+
+const Login = () => {
+  return (
+    <div className='layout'>
+    <img src={require('../../asets/Mask group.png')} className='logo' alt="" srcset="" />
+      
+    </div>
+  )
+}
+
+export default Login
