8545917652a354f936742e72934802220fb7fb51
laxitsolanki <solankilaxit963@gmail.com>
2023-09-25 19:21:01 +0530

    step 1 reaning

--- src/Components/buyear/Buyearstep1.js
+++ src/Components/buyear/Buyearstep1.js
@@ -34,19 +34,79 @@ const Buyearstep1 = () => {
   <br/>
   <form action="">
   <div className="h1">Business Details</div>
-  </form>
+
 <img src={require('../../asets/Group 427320789.png')} alt="" srcset="" />
-  <input type="text" id="fname" name="fname" className='inputCompny' placeholder='Company name'/><br/>
-  <img src={require('../../asets/Group 427320789.png')} alt="" srcset="" />
-  <input type="text" id="fname" name="fname" className='inputBussines' placeholder='Business category'/><br/>
-  
-                {/* <Link to="/Buyear">
-                        <i class="fa-sharp fa-solid fa-eye text-black"></i>
-                      </Link> */}
-                {/* <Link to="/Buyear" className="create">create account</Link><br /><br /><br /> */}
+<input type="text" name='iinputcompny' className='inputCompny' placeholder='Compny name' /><br/>
+  <img src={require('../../asets/Group 427320766.png')} alt="" srcset="" />
+  <input list="browsers" name="inputcompny" className='inputCompny' placeholder='Business category' /><br/>
+  <datalist id="browsers">
+    <option value="zestbrains"></option>
+    <option value="plus"></option>
+    <option value="Chrome"></option>
+    <option value="xyz"></option>
+    <option value="Safari"></option>
+  </datalist>
+  <img src={require('../../asets/Group 427321386.png')} alt="" />
+  <input list="browsers" name="inputcompny" className='inputCompny' placeholder='Business category' /><br/>
+  <datalist id="browsers">
+    <option value="zestbrains"></option>
+    <option value="plus"></option>
+    <option value="Chrome"></option>
+    <option value="xyz"></option>
+    <option value="Safari"></option>
+  </datalist>
+  </form>
+  <form action="">
+  <div className="h1">Personal Details</div>
+  <img src={require('../../asets/Profile.png')} alt="" />
+  <input type="text" name='iinputcompny' className='inputCompny' placeholder='First name' /><br/>
+  <img src={require('../../asets/Profile.png')} alt="" />
+  <input type="text" name='iinputcompny' className='inputCompny' placeholder='Last name' /><br/>
+
+  <div className='inputnumber-main'>
+  <img src={require('../../asets/Call.png')} alt="" className='iconphone' />
+  <input list="phone" name="phone" className='inputnumber' placeholder='91+' required=""
+  pattern="[0-9]{10}" /><br/>
+  <datalist id="phone">
+    <option value="91+"></option>
+    <option value="92+"></option>
+    <option value="93+"></option>
+    <option value="94+"></option>
+    <option value="85+"></option>
+  </datalist>
+    <input type="phone" className='number' placeholder='Mobile number'  /><br/>
+</div>
+<img src={require('../../asets/Location.png')} alt="" className='address' />
+    <input type="phone" className='Address1' placeholder='Address1'  /><br/>
+    <img src={require('../../asets/Location.png')} alt="" className='address' />
+    <input type="phone" className='Address1' placeholder='Address1'  /><br/>
+
+    <img src={require('../../asets/Group 427321426.png')} alt="" className='cityimg' />
+    <input type="phone" className='city' placeholder='city/Town'  /><br/>
+    <input list="phone" name="state" className='state' placeholder='state' required=""
+  pattern="[0-9]{10}" /><br/>
+  <datalist id="phone">
+    <option value="91+"></option>
+    <option value="92+"></option>
+    <option value="93+"></option>
+    <option value="94+"></option>
+    <option value="85+"></option>
+  </datalist>
+
+  <input list="phone" name="country" className='country' placeholder='country' required=""
+  pattern="[0-9]{10}" /><br/>
+  <datalist id="phone">
+    <option value="91+"></option>
+    <option value="92+"></option>
+    <option value="93+"></option>
+    <option value="94+"></option>
+    <option value="85+"></option>
+  </datalist>
+  </form>
                 <span className="spana"><a href="#" className="logingest">login </a>
                 As Guest</span>
             </form>
+
     </div>
     </div>
   )
